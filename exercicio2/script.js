61f4427b44297514b608b1753cb8cfcd39cd6cf3
Joanadayse <daysejoana3@gmail.com>
2022-08-19 16:48:41 -0300

    feito

---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("digite um numero para multiplicar"))
+ tabuada= [1,2,3,4,5,6,7,8,9,10]
+
+
+        for (let i in tabuada ){
+        console.log(tabuada[i]*numero)
+        }
---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+
+ranking = 0;
+for (const paises of maioresPaises){
+    ranking++;
+    console.log(`${ranking} ${paises}`);
+}
